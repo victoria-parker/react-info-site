761ec4bd6428932ebd078b411f0d15bf3d95e548
victoria-parker <victoriaparkerb@gmail.com>
2022-01-07 16:51:09 +0000

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <footer>
+                <small>© 2022 Victoria Parker development. All rights reserved.</small>
+            </footer>
+    )
+}
