a7586e2a79f4a58941efc96179b8c9cd3479f9e1
victoria-parker <victoriaparkerb@gmail.com>
2022-01-08 16:52:58 +0000

    main done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react'
 import "./style.css"
 import NavBar from "./components/NavBar"
 import Main from './components/Main'
-import Footer from './components/Footer'
+
 
 export default function App(){
     return (
         <div className="container">
             <NavBar/>
             <Main/>
-            <Footer/>
         </div>)
 }
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer(){
-    return (
-        <footer>
-                <small>© 2022 Victoria Parker development. All rights reserved.</small>
-            </footer>
-    )
-}
